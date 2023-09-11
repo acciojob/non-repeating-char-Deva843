7a1f7f14a1bb0fd005b51a8aef445103253c11c6
DevaKumar <137104336+Deva843@users.noreply.github.com>
2023-09-11 17:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str.length<=1){
+		return null;
+	}
+	
+	for(let i =0;i<str.length-1;i++){
+		if(str.charAt(i)=str.charAt(i+1)){
+			continue;
+		}
+		else{
+			return str.charAt(i);
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
